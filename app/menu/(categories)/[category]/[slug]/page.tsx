025644fc38a8d150df56a65d56ba60bb04e59660
SayKham99 <x5292621@gmail.com>
2022-12-10 20:23:12 +0500

    'fix:optimized previous commit'

--- app/menu/(categories)/[category]/[slug]/page.tsx
+++ app/menu/(categories)/[category]/[slug]/page.tsx
@@ -31,312 +31,35 @@ export default async function Page({ params }: Props) {
 				</h1>
 
 				<h2 className='category-item-header'>{products[0].category}</h2>
-
-				<h2 className='category-item-header'>{data[0]}</h2>
-				<hr />
-				<div className='product-item-wrapper'>
-					{products.map(product => {
-						return data[0] == product.mc_category ? (
-							<>
-								<Link
-									className='product-item'
-									key={product.id}
-									href={`${product.mc_slug}/${product.slug}`}
-								>
-									<div className='product-item-inner'>
-										<div className='product-view-image'>
-											<img src={product.image?.asset.url} alt={product.name} />
-										</div>
-										<p className='text-[19px] text-neutral-800'>
-											{product.name}
-										</p>
+				{data.map(category => (
+					<div key={category}>
+						<h2 className='category-item-header'>{category}</h2>
+						<hr />
+						<div className='product-item-wrapper'>
+							{products.map(product => {
+								return category == product.mc_category ? (
+									<div key={product.id} className='product-item'>
+										<Link href={`${product.mc_slug}/${product.slug}`}>
+											<div className='product-item-inner'>
+												<div className='product-view-image'>
+													<img
+														src={product.image?.asset.url}
+														alt={product.name}
+													/>
+												</div>
+												<p className='text-[19px] text-neutral-800'>
+													{product.name}
+												</p>
+											</div>
+										</Link>
 									</div>
-								</Link>
-							</>
-						) : (
-							''
-						)
-					})}
-				</div>
-				<div className={`${data[1] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[1]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[1] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
+								) : (
+									''
+								)
+							})}
+						</div>
 					</div>
-				</div>
-				<div className={`${data[2] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[2]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[2] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[3] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[3]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[3] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[4] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[4]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[4] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[5] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[5]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[5] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[6] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[6]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[6] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[7] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[7]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[7] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[8] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[8]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[8] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
-				<div className={`${data[9] == undefined ? 'hidden' : ''}`}>
-					<h2 className='category-item-header'>{data[9]}</h2>
-					<hr />
-					<div className='product-item-wrapper'>
-						{products.map(product => {
-							return data[9] == product.mc_category ? (
-								<>
-									<Link
-										className='product-item'
-										key={product.id}
-										href={`${product.mc_slug}/${product.slug}`}
-									>
-										<div className='product-item-inner'>
-											<div className='product-view-image'>
-												<img
-													src={product.image?.asset.url}
-													alt={product.name}
-												/>
-											</div>
-											<p className='text-[19px] text-neutral-800'>
-												{product.name}
-											</p>
-										</div>
-									</Link>
-								</>
-							) : (
-								''
-							)
-						})}
-					</div>
-				</div>
+				))}
 			</aside>
 		</>
 	)
